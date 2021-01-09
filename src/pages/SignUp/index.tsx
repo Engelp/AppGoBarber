ddadd5af9166e5901b58a41474eb60c6684e3a0d
Elson Pinheiro Silva <engelp@gmail.com>
2021-01-08 22:04:03 -0300

    Navegacao criar conta

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,9 +1,50 @@
 import React from 'react';
+import {Image, View, KeyboardAvoidingView, Platform} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+import {useNavigation} from '@react-navigation/native';
 
-import {Container} from './styles';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import logoImg from '../../assets/logo.png';
+
+import {Container, Title, BackToSignIn, BackToSignInText} from './styles';
 
 const SignUp: React.FC = () => {
-  return <Container />;
+  const navigation = useNavigation();
+
+  return (
+    <>
+      <KeyboardAvoidingView
+        style={{flex: 1}}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled>
+        <Container>
+          <Image source={logoImg} />
+
+          <View>
+            <Title>Crie sua conta</Title>
+          </View>
+
+          <Input name="name" icon="user" placeholder="nome" />
+
+          <Input name="email" icon="mail" placeholder="email" />
+
+          <Input name="password" icon="lock" placeholder="Senha" />
+
+          <Button onPress={() => {}}>Entrar</Button>
+        </Container>
+      </KeyboardAvoidingView>
+
+      <BackToSignIn
+        onPress={() => {
+          navigation.goBack();
+        }}>
+        <Icon name="arrow-left" size={20} color="#fff" />
+        <BackToSignInText>Voltar para a tela inicial</BackToSignInText>
+      </BackToSignIn>
+    </>
+  );
 };
 
 export default SignUp;
